680234d8b04d37a43ad9d21d35600bfab1678ee7
Ignacio Del Nardo <ignacioguerra111@mail.com>
2017-02-01 16:12:34 -0400

    New Idents

--- src/utils/DatasourceLanguage.js
+++ src/utils/DatasourceLanguage.js
@@ -1,38 +1,38 @@
 export default {
-    translations: {
-        // English translation
-        'en': {
-            'table': {
-                'label_limits': 'Show',
-                'label_search': 'Search',
-                'placeholder_search': 'Type to search..',
-                'records_not_found': 'No records found'
-            },
-            'pagination': {
-                'label_show': 'Showing',
-                'label_to': 'to',
-                'label_of': 'of',
-                'label_entries': 'entries',
-                'btn_first': 'First',
-                'btn_last': 'Latest'
-            }
-        },
-        // Spanish translation
-        'es': {
-            'table': {
-                'label_limits': 'Mostrar',
-                'label_search': 'Buscar',
-                'placeholder_search': 'Buscar ..',
-                'records_not_found': 'No se encontraron registros.'
-            },
-            'pagination': {
-                'label_show': 'Mostrando',
-                'label_to': 'a',
-                'label_of': 'de',
-                'label_entries': 'registros',
-                'btn_first': 'Primero',
-                'btn_last': 'Último'
-            }
-        }
+  translations: {
+    // English translation
+    'en': {
+      'table': {
+        'label_limits': 'Show',
+        'label_search': 'Search',
+        'placeholder_search': 'Type to search..',
+        'records_not_found': 'No records found'
+      },
+      'pagination': {
+        'label_show': 'Showing',
+        'label_to': 'to',
+        'label_of': 'of',
+        'label_entries': 'entries',
+        'btn_first': 'First',
+        'btn_last': 'Latest'
+      }
+    },
+    // Spanish translation
+    'es': {
+      'table': {
+        'label_limits': 'Mostrar',
+        'label_search': 'Buscar',
+        'placeholder_search': 'Buscar ..',
+        'records_not_found': 'No se encontraron registros.'
+      },
+      'pagination': {
+        'label_show': 'Mostrando',
+        'label_to': 'a',
+        'label_of': 'de',
+        'label_entries': 'registros',
+        'btn_first': 'Primero',
+        'btn_last': 'Último'
+      }
     }
+  }
 }
